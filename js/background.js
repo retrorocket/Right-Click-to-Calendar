05e20bd2015dc948278a5ecfa3ed92e100f8abf3
retrorocket <unrealenemy@gmail.com>
2015-04-05 18:54:43 +0900

    using panel

--- js/background.js
+++ js/background.js
@@ -1,87 +1,42 @@
 //コンテキストメニューに登録
 var parentId = chrome.contextMenus.create({
-	"title" : "選択したテキストをカレンダーに投稿",
-	"type" : "normal",
-	"contexts" : ["selection"],//テキストを選択した時のみ
-	"onclick" : getClickHandler()
+    "title": "選択したテキストをカレンダーに投稿",
+    "type": "normal",
+    "contexts": ["selection"], //テキストを選択した時のみ
+    "onclick": getClickHandler()
 });
 
 //イベントウィンドウに引き渡す値
-var args = new Object();
+var args = null;
 
 function getClickHandler() {
-	return function(info, tab) {
+    return function (info, tab) {
+        args = null;
+        if (!localStorage["calenId"]) {
+            alert("オプションページで認証を行ってください");
+            chrome.tabs.create({
+                "url": chrome.extension.getURL("options.html"),
+            });
+            return false;
+        }
 
-		if(!localStorage["calenId"])
-		{
-			alert ("オプションページで認証を行ってください");
-			chrome.tabs.create({
-  			  "url": chrome.extension.getURL("options.html"),
-			});
-			return false;
-		}
+        var stext = info.selectionText; //現在選択しているテキスト
+        stext = stext.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function (s) { //全角英数を半角に変換
+            return String.fromCharCode(s.charCodeAt(0) - 65248);
+        });
 
-		var stext=info.selectionText; //現在選択しているテキスト
-		stext = stext.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {//全角英数を半角に変換
-			return String.fromCharCode(s.charCodeAt(0) - 65248);
-		});
-
-		if(localStorage["expSwitch"]) {
-			args = expDate(stext);
-			if(args) { // 正規表現に合致した場合のみ
-				chrome.windows.create({"url":"setEvent.html", "width":580, "height":810, "type": "panel"});
-				return;
-			}
-		}
-
-		// 年月日・曜日・時分秒の取得
-		var d = new Date();	
-
-		/**正規表現で日付処理（てきとうすぎて使えない．関数を作ってそこで処理すべき）**/
-		var mon = d.getMonth() + 1;
-		var day =  d.getDate();
-		var syear =  d.getFullYear();
-
-		var shour = d.getHours();
-		var smin = d.getMinutes();
-
-		var title = stext;
-
-		var m = stext.match(/(\d{1,2})(\/|月)(\d{1,2})/);//日付
-		if(m){
-			mon = m[1];
-			day = m[3];
-		}
-
-		var rr = stext.match(/(\d{1,2})(:|時)/);//開始時刻
-		if(rr){
-			shour = rr[1];
-			smin = 0;
-		}
-
-		var r = stext.match(/(\d{1,2})(:|時)(\d{1,2})/);//開始時刻
-		if(r){
-			shour = r[1];
-			smin = r[3];
-		}
-
-		var t = stext.match(/(\d+| |\n)(\S+)$/);//タイトル
-		if(t){
-			title = t[2];
-		}
-
-		args = {
-			"start":{"year":syear, "month":mon, "day":day,
-				 "hour":shour, "min":smin},
-			"end":{"year":syear, "month":mon, "day":day,
-				"hour":shour, "min":smin},
-			"title":title,
-			"detail":"",
-			"selected_text":stext
-		};
-
-		//イベント設定ウィンドウを呼び出す
-		chrome.windows.create({"url":"setEvent.html", "width":580, "height":810, "type": "panel"});
-	}
+        if (localStorage["expSwitch"]) {
+            args = expDate(stext);
+        }
+        if(!args) {
+            args = expDefault(stext);
+        }
+        //イベント設定ウィンドウを呼び出す
+        chrome.windows.create({
+            "url": "setEvent.html",
+            "width": 580,
+            "height": 810,
+            "type": "panel"
+        });
+    }
 }
-
--- js/setEvent.js
+++ js/setEvent.js
@@ -1,277 +1,320 @@
 var bg = chrome.extension.getBackgroundPage();
 
-function isValidDate(y,m,d){
-	y = parseInt(y, 10);
-	m = parseInt(m, 10);
-	d = parseInt(d, 10);
-	var di = new Date(y,m-1,d);
-	if(di.getFullYear() == y && di.getMonth() == m-1 && di.getDate() == d){
-		return true;
-	}
-	return false;
+function isValidDate(y, m, d) {
+    y = parseInt(y, 10);
+    m = parseInt(m, 10);
+    d = parseInt(d, 10);
+    var di = new Date(y, m - 1, d);
+    if (di.getFullYear() == y && di.getMonth() == m - 1 && di.getDate() == d) {
+        return true;
+    }
+    return false;
 }
 
-function checkRange(start, shour, smin, end, ehour, emin){
-	try{
+function checkRange(start, shour, smin, end, ehour, emin) {
+    try {
 
-		if(!time_checker(shour,smin) || !time_checker(ehour,emin)){
-			return false;
-		}
-		var s_d  = start + " " + checker(shour) + " " + checker(smin);
-		var e_d  = end + " " + checker(ehour) + " " + checker(emin);
+        if (!time_checker(shour, smin) || !time_checker(ehour, emin)) {
+            return false;
+        }
+        var s_d = start + " " + checker(shour) + " " + checker(smin);
+        var e_d = end + " " + checker(ehour) + " " + checker(emin);
 
-		var sDate = $.exDate(s_d, "yyyy-mm-dd hh mi");
-		var eDate = $.exDate(e_d, "yyyy-mm-dd hh mi");
+        var sDate = $.exDate(s_d, "yyyy-mm-dd hh mi");
+        var eDate = $.exDate(e_d, "yyyy-mm-dd hh mi");
 
-		if( (eDate.getTime() - sDate.getTime() )  <  0 ){
-			return false;
-		}
+        if ((eDate.getTime() - sDate.getTime()) < 0) {
+            return false;
+        }
 
-		return true;
-	}
-	catch( e ){
-		return false;
-	}
+        return true;
+    } catch (e) {
+        return false;
+    }
 }
 
-function time_checker(h, m){
-	//console.log(h,m);
-	if(parseInt(h,10) < 24 && parseInt(h,10) >= 0 && parseInt(m,10) < 60 && parseInt(m,10) >= 0){
-		return true
-	}
-	return false;
+function time_checker(h, m) {
+    //console.log(h,m);
+    if (parseInt(h, 10) < 24 && parseInt(h, 10) >= 0 && parseInt(m, 10) < 60 && parseInt(m, 10) >= 0) {
+        return true
+    }
+    return false;
 }
 
-function checker(ch){
-	//console.log(ch + "→" +parseInt(ch,10));
-	if(parseInt(ch,10) < 10 ){
-		ch= "0"+ parseInt(ch,10);
-	}
-	return ch;
+function checker(ch) {
+    //console.log(ch + "→" +parseInt(ch,10));
+    if (parseInt(ch, 10) < 10) {
+        ch = "0" + parseInt(ch, 10);
+    }
+    return ch;
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-	chrome.identity.getAuthToken({ 'interactive': true }, 
-		function(access_token) {
-			var xhr = new XMLHttpRequest();
-		
-			xhr.onreadystatechange =  function() {
-				if(xhr.readyState === 4) {
-					if(xhr.status === 200) {
-						var data = JSON.parse(xhr.responseText);
-						//console.log(data);
-						var list = data.items;
-						for (var i = 0; i < list.length; i++){
-							$("#example").append($('<option>').html(list[i].summary).val(list[i].id));
-						}
-	
-						$("#example").val(localStorage["calenId"]);
-					}
-					else if(xhr.status === 401){
-						var data  =  JSON.parse(xhr.responseText);
-						chrome.identity.removeCachedAuthToken(
-           						{ 'token': access_token },
-							function(){
-								alert("無効なアクセストークンを削除しました。オプションページで再認証してください。" + data.error.code + " : " + data.error.message);
-								window.close();
-							});
-						return;
-					}
-					else {
-						var data = JSON.parse(xhr.responseText);
-						//console.log(data);
-						alert("リストの取得に失敗しました。ウインドウを閉じます。");
-						window.close();
-					}
-				}
-			}
-			xhr.open('GET',
-				"https://www.googleapis.com/calendar/v3/users/me/calendarList?minAccessRole=owner",
-			true);
-			xhr.setRequestHeader('Content-Type', 'application/json');
-			xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-			xhr.send(null);
-		}
-	);
+    chrome.identity.getAuthToken({
+            'interactive': true
+        },
+        function (access_token) {
+            var xhr = new XMLHttpRequest();
 
-	$( "#from" ).datepicker({
-		dateFormat: "yy-mm-dd",
-		onSelect: function( selectedDate ) {
-			$( "#to" ).datepicker( "option", "minDate", selectedDate );
-		}
-	});
-	$( "#to" ).datepicker({
-		dateFormat: "yy-mm-dd",
-		onSelect: function( selectedDate ) {
-			$( "#from" ).datepicker( "option", "maxDate", selectedDate );
-		}
-	});
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        var data = JSON.parse(xhr.responseText);
+                        //console.log(data);
+                        var list = data.items;
+                        for (var i = 0; i < list.length; i++) {
+                            $("#example").append($('<option>').html(list[i].summary).val(list[i].id));
+                        }
 
-	jQuery("#formID").validationEngine('attach');
-	//window.returnValue = false;
+                        $("#example").val(localStorage["calenId"]);
+                    } else if (xhr.status === 401) {
+                        var data = JSON.parse(xhr.responseText);
+                        chrome.identity.removeCachedAuthToken({
+                                'token': access_token
+                            },
+                            function () {
+                                //alert("無効なアクセストークンを削除しました。オプションページで再認証してください。" + data.error.code + " : " + data.error.message);
+                                swal({
+                                        title: "invalid AccessToken!",
+                                        text: "無効なアクセストークンを削除しました。オプションページで再認証してください。" + data.error.code + " : " + data.error.message,
+                                        animation: false
+                                    },
+                                    function () {
+                                        window.close();
+                                    });
+                                //window.close();
+                            });
+                        return;
+                    } else {
+                        var data = JSON.parse(xhr.responseText);
+                        //console.log(data);
+                        //alert("リストの取得に失敗しました。ウインドウを閉じます。");
+                        swal({
+                                title: "Acquisition failure!",
+                                text: "リストの取得に失敗しました。ウインドウを閉じます",
+                                animation: false
+                            },
+                            function () {
+                                window.close();
+                            });
+                        return;
+                    }
+                }
+            }
+            xhr.open('GET',
+                "https://www.googleapis.com/calendar/v3/users/me/calendarList?minAccessRole=owner",
+                true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+            xhr.send(null);
+        }
+    );
 
-	var start_year  = bg.args.start.year;
-	var start_month = bg.args.start.month;
-	var start_day = bg.args.start.day;
-	var start_hour = bg.args.start.hour;
-	var start_min = bg.args.start.min;
+    $("#from").datepicker({
+        dateFormat: "yy-mm-dd",
+        onSelect: function (selectedDate) {
+            $("#to").datepicker("option", "minDate", selectedDate);
+        }
+    });
+    $("#to").datepicker({
+        dateFormat: "yy-mm-dd",
+        onSelect: function (selectedDate) {
+            $("#from").datepicker("option", "maxDate", selectedDate);
+        }
+    });
 
-	var end_year = bg.args.end.year;
-	var end_month = bg.args.end.month;
-	var end_day = bg.args.end.day;
-	var end_hour = bg.args.end.hour;
-	var end_min = bg.args.end.min;
+    jQuery("#formID").validationEngine('attach');
+    //window.returnValue = false;
 
-	if(isValidDate(start_year,start_month,start_day)){
-		$("#from").val(start_year + "-" + checker(start_month) + "-" + checker(start_day));
-	}
+    var start_year = bg.args.start.year;
+    var start_month = bg.args.start.month;
+    var start_day = bg.args.start.day;
+    var start_hour = bg.args.start.hour;
+    var start_min = bg.args.start.min;
 
-	if(isValidDate(end_year,end_month,end_day)){
-		$("#to").val(end_year + "-" + checker(end_month) + "-" + checker(end_day));
-	}
+    var end_year = bg.args.end.year;
+    var end_month = bg.args.end.month;
+    var end_day = bg.args.end.day;
+    var end_hour = bg.args.end.hour;
+    var end_min = bg.args.end.min;
 
-	if(time_checker(start_hour,start_min)){
-		$("#f_hour").val(checker(start_hour));
-		$("#f_min").val(checker(start_min));
-	}
+    if (isValidDate(start_year, start_month, start_day)) {
+        $("#from").val(start_year + "-" + checker(start_month) + "-" + checker(start_day));
+    }
 
-	if(time_checker(end_hour,end_min)){
-		$("#e_hour").val(checker(end_hour));
-		$("#e_min").val(checker(end_min));
-	}
+    if (isValidDate(end_year, end_month, end_day)) {
+        $("#to").val(end_year + "-" + checker(end_month) + "-" + checker(end_day));
+    }
 
-	$("#tit").val(bg.args.title);
+    if (time_checker(start_hour, start_min)) {
+        $("#f_hour").val(checker(start_hour));
+        $("#f_min").val(checker(start_min));
+    }
 
-	$("#main_text").val(bg.args.selected_text);
+    if (time_checker(end_hour, end_min)) {
+        $("#e_hour").val(checker(end_hour));
+        $("#e_min").val(checker(end_min));
+    }
 
-	$("#detail").val(bg.args.detail);
+    $("#tit").val(bg.args.title);
 
-	$("#sub").click(function(){
+    $("#main_text").val(bg.args.selected_text);
 
-		var range_bool = false;
+    $("#detail").val(bg.args.detail);
 
-		var from = $("#from").val();
-		var to = $("#to").val();
+    $("#sub").click(function () {
 
-		if ($('#check').attr('checked')){
-			var myDate = $.exDate(to, "yyyy-mm-dd");
-			//console.log(myDate);
-			myDate.setDate(myDate.getDate() + 1);
-			to = myDate.toChar('yyyy-mm-dd');
+        var range_bool = false;
 
-			range_bool = checkRange(from,"0","0",to,"0","0");
+        var from = $("#from").val();
+        var to = $("#to").val();
 
-		}
-		else{
-			range_bool = checkRange(from, $("#f_hour").val(), $("#f_min").val(), to, $("#e_hour").val(), $("#e_min").val());
-		}
+        if ($('#check').attr('checked')) {
+            var myDate = $.exDate(to, "yyyy-mm-dd");
+            //console.log(myDate);
+            myDate.setDate(myDate.getDate() + 1);
+            to = myDate.toChar('yyyy-mm-dd');
 
-		if(range_bool){
-			var obj = {
-				title :$("#tit").val(),
-				detail :$("#detail").val(),
-				f_mon : from,
+            range_bool = checkRange(from, "0", "0", to, "0", "0");
 
-				f_hour : checker($("#f_hour").val()),
-				f_min : checker($("#f_min").val()),
+        } else {
+            range_bool = checkRange(from, $("#f_hour").val(), $("#f_min").val(), to, $("#e_hour").val(), $("#e_min").val());
+        }
 
-				check : $('#check').attr('checked'),
+        if (range_bool) {
+            var obj = {
+                title: $("#tit").val(),
+                detail: $("#detail").val(),
+                f_mon: from,
 
-				e_hour : checker($("#e_hour").val()),
-				e_min: checker($("#e_min").val()),
+                f_hour: checker($("#f_hour").val()),
+                f_min: checker($("#f_min").val()),
 
-				e_mon : to,
-	
-				calen : $("#example").val()
-			};
+                check: $('#check').attr('checked'),
 
-			//イベント投稿
-			add_event(obj);
-		}
-		else{
-			alert ("日時が正しくありません");
-		}
-	});
+                e_hour: checker($("#e_hour").val()),
+                e_min: checker($("#e_min").val()),
 
-	$("#check").click(
-			function () {
-				if (!this.checked) {
-					$("input.time").removeAttr("disabled");
-				}
-				else {
-					$("input.time").attr("disabled", true);
-				}
-			});
-	});
+                e_mon: to,
 
-function add_event(returnValue){
-	chrome.identity.getAuthToken({ 'interactive': true }, 
-		function(access_token) {
-			/**開始～終了日時設定**/
+                calen: $("#example").val()
+            };
 
-			var st ="";//開始日時
-			var en ="";//終了日時
+            //イベント投稿
+            add_event(obj);
+        } else {
+            swal({
+                title: "invalid date!",
+                text: "日時が正しくありません",
+                animation: false
+            });
+            //alert ("日時が正しくありません");
+        }
+    });
 
-			var start_array;
-			var end_array;
+    $("#check").click(
+        function () {
+            if (!this.checked) {
+                $("input.time").removeAttr("disabled");
+            } else {
+                $("input.time").attr("disabled", true);
+            }
+        });
+});
 
-			if(returnValue["check"]){//終日設定
-				st = returnValue["f_mon"];
-				en = returnValue["e_mon"];
-				start_array = {"date":st};
-				end_array = {"date":en};
-			}
-			else {
-				st = returnValue["f_mon"] + "T" + returnValue["f_hour"] + ":" + returnValue["f_min"] + ":00.000+09:00";
-				en = returnValue["e_mon"] + "T" + returnValue["e_hour"] + ":" + returnValue["e_min"] + ":00.000+09:00";
-				start_array = {"dateTime":st};
-				end_array = {"dateTime":en};
-			}
+function add_event(returnValue) {
+    chrome.identity.getAuthToken({
+            'interactive': true
+        },
+        function (access_token) {
+            /**開始～終了日時設定**/
 
-			/**設定終わり**/
+            var st = ""; //開始日時
+            var en = ""; //終了日時
 
-			var body = JSON.stringify({
-				"description": returnValue["detail"],
-				"summary": returnValue["title"],
-				"transparency": "opaque",
-				"status": "confirmed",
-				"start" : start_array,
-				"end": end_array
-			});
+            var start_array;
+            var end_array;
 
-			var xhr = new XMLHttpRequest();
-			xhr.onreadystatechange =  function() {
-				if(xhr.readyState === 4) {
-					if(xhr.status === 200) {
-						alert("【"+ returnValue["title"] + "】" +"を登録しました");
-						window.close();
-					}
-					else if(xhr.status === 401){
-						var data = JSON.parse(xhr.responseText);
-						chrome.identity.removeCachedAuthToken(
-           						{ 'token': access_token },
-							function(){
-								alert("無効なアクセストークンを削除しました。オプションページで再認証してください。" + data.error.code + " : " + data.error.message);
-							});
-						return;
-					}
-					else{
-						var data = JSON.parse(xhr.responseText);
-						alert("(Error)" + data.error.code + " : " + data.error.message);
-					}
-				}
-			}
-			var calenid = returnValue["calen"];
-			xhr.open('POST',
-				"https://www.googleapis.com/calendar/v3/calendars/"+ calenid +"/events",
-				true);
+            if (returnValue["check"]) { //終日設定
+                st = returnValue["f_mon"];
+                en = returnValue["e_mon"];
+                start_array = {
+                    "date": st
+                };
+                end_array = {
+                    "date": en
+                };
+            } else {
+                st = returnValue["f_mon"] + "T" + returnValue["f_hour"] + ":" + returnValue["f_min"] + ":00.000+09:00";
+                en = returnValue["e_mon"] + "T" + returnValue["e_hour"] + ":" + returnValue["e_min"] + ":00.000+09:00";
+                start_array = {
+                    "dateTime": st
+                };
+                end_array = {
+                    "dateTime": en
+                };
+            }
 
-			xhr.setRequestHeader('Content-Type', 'application/json');
-			xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-			xhr.send(body);
-		}
-	);
+            /**設定終わり**/
+
+            var body = JSON.stringify({
+                "description": returnValue["detail"],
+                "summary": returnValue["title"],
+                "transparency": "opaque",
+                "status": "confirmed",
+                "start": start_array,
+                "end": end_array
+            });
+
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        //alert("【"+ returnValue["title"] + "】" +"を登録しました");
+                        swal({
+                                title: "succeed!",
+                                text: "【" + returnValue["title"] + "】" + "を登録しました",
+                                animation: false
+                            },
+                            function () {
+                                window.close();
+                            });
+                        //window.close();
+                    } else if (xhr.status === 401) {
+                        var data = JSON.parse(xhr.responseText);
+                        chrome.identity.removeCachedAuthToken({
+                                'token': access_token
+                            },
+                            function () {
+                                //alert("無効なアクセストークンを削除しました。オプションページで再認証してください。" + data.error.code + " : " + data.error.message);
+                                swal({
+                                    title: "invalid AccessToken",
+                                    text: "無効なアクセストークンを削除しました。オプションページで再認証してください。" + data.error.code + " : " + data.error.message,
+                                    animation: false
+                                });
+                            });
+                        return;
+                    } else {
+                        var data = JSON.parse(xhr.responseText);
+                        //alert("(Error)" + data.error.code + " : " + data.error.message);
+                        swal({
+                            title: "error!",
+                            text: data.error.code + " : " + data.error.message,
+                            animation: false
+                        });
+                        return;
+                    }
+                }
+            }
+            var calenid = returnValue["calen"];
+            xhr.open('POST',
+                "https://www.googleapis.com/calendar/v3/calendars/" + calenid + "/events",
+                true);
+
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+            xhr.send(body);
+        }
+    );
 }
-
-
