f22db19b9c9282a50bda6a6f92d8c72aafabd161
retrorocket <mail@retrorocket.biz>
2020-10-29 14:59:44 +0900

    CircleCI対応

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   // Add this line to your Jest config
   setupFilesAfterEnv: ['./jest.setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['app/assets/js/*.{js,ts}'],
+  coverageDirectory: 'coverage',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'jest tests',
+        outputDirectory: 'reports/jest',
+        outputName: 'js-test-results.xml',
+        classNameTemplate: '{classname}-{title}',
+        titleTemplate: '{classname}-{title}',
+        ancestorSeparator: ' › ',
+      },
+    ],
+  ],
 }
