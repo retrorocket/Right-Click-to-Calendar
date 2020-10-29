873233c78ff97211a3f19bb5b79bcb30daf8e32d
りゅー <1858830+retrorocket@users.noreply.github.com>
2020-10-30 01:04:24 +0900

    GitHub ActionsからCircleCIに移行する (#24)
    * CircleCIに対応
    
    * 修正
    
    * 修正
    
    * 修正
    
    * update-yarn削除
    
    * jestのリポーターをjunitに修正
    
    * jest.config.js修正
    
    * OUTPUT修正
    
    * jest-junit追加
    
    * CircleCI対応
    
    * CircleCI対応
    
    * テストをわざと失敗させる
    
    * ESLintの追加
    
    * テストを元に戻す
    
    * コマンドの修正
    
    * ESLintが動作するかテストする
    
    * Lintが失敗するコードを仕込む
    
    * CircleCIなんもわからん
    
    * CircleCI向けconfigファイルの修正
    
    * ssh-keyscanにはポート番号が必要って言ってたじゃないですか
    
    * 余計な行を削除
    
    * 変数名誤り修正
    
    * persist_to_workspaceのパス修正
    
    * パスの修正
    
    * 圧縮先パス修正
    
    * 圧縮先のパス修正
    
    * GitHub ActionsからCircleCIに移行
    
    * コメントの整理
    
    * 実行ブランチの変更
    
    * コメントの修正

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // Add this line to your Jest config
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['app/assets/js/*.{js,ts}'],
+  coverageDirectory: 'coverage',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'jest tests',
+        outputDirectory: 'reports/jest',
+        outputName: 'js-test-results.xml',
+        ancestorSeparator: ' › ',
+      },
+    ],
+  ],
+}
