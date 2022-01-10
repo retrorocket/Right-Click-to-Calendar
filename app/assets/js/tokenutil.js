a63c6a0a4354b0eb240017aed0ff18880b45fed5
retrorocket <mail@retrorocket.biz>
2022-01-10 15:25:06 +0900

    remove comment

--- app/assets/js/tokenutil.js
+++ app/assets/js/tokenutil.js
@@ -2,7 +2,7 @@
 
 const REDIRECT_URL = chrome.identity.getRedirectURL("/oauth2");
 const CLIENT_ID = "94384066361-kgpm35l8cdcn4kqrd09tob7sssulnj1c.apps.googleusercontent.com"
-const SCOPES = ["https://www.googleapis.com/auth/calendar.events", "https://www.googleapis.com/auth/calendar.readonly"]; // 'https://www.googleapis.com/auth/drive.appfolder'
+const SCOPES = ["https://www.googleapis.com/auth/calendar.events", "https://www.googleapis.com/auth/calendar.readonly"];
 const AUTH_URL = `https://accounts.google.com/o/oauth2/auth?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(
   REDIRECT_URL
 )}&scope=${encodeURIComponent(SCOPES.join(" "))}`;
