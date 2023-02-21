5014f19df817023a456bc058c786f862be9d7c4c
retrorocket <1858830+retrorocket@users.noreply.github.com>
2023-02-22 00:28:43 +0900

    use authorization code flow

---
+++ app/assets/js/gettoken.js
@@ -0,0 +1,38 @@
+export const getToken = () => {
+
+  const senddata =
+  {
+    "app": "femihkgadmhfmdlkjjfjcgleppfggadk"
+  };
+
+  return fetch("https://client.retrorocket.biz/token", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(senddata)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("failed to get request token");
+      }
+      return response.json();
+    })
+    .then(data => {
+      alert(data.access_token);
+      if (data.access_token) {
+        chrome.storage.local.set({ "accessToken": data.access_token }, () => {
+          alert("アクセストークンを取得しました。ウィンドウを閉じます。\n\nオプションページからトークンを取得した場合：\nウィンドウを閉じたあと、オプションページをリロードしてください。\n\nイベント設定ウィンドウに登録対象のカレンダーが表示されていない場合：\nウィンドウを閉じたあと、イベント設定ウィンドウ内の「再取得」ボタンをクリックしてください。クリック後予定が登録できるようになります。");
+          window.close();
+        });
+      } else {
+        throw new Error("failed to get request token");
+      }
+    })
+    .catch(() => {
+      alert("アクセストークンを取得できませんでした。このウィンドウを閉じてオプションページから再度登録を行ってください。");
+      window.close();
+    });
+}
+
+getToken();
