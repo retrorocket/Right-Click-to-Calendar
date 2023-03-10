180fccc03e46e4190f1a3e8e71dabf4ca13b57ab
retrorocket <1858830+retrorocket@users.noreply.github.com>
2023-03-10 15:16:49 +0900

    英語月を数字に変換する

--- __tests__/expdefaultTest.js
+++ __tests__/expdefaultTest.js
@@ -110,3 +110,115 @@ test("デフォルト正規表現テスト_ISOformat", () => {
     }
   )
 });
+
+test("デフォルト正規表現テスト_月が英語表記_US1", () => {
+  localStorage.removeItem("detailSwitch");
+  expect(expDefault("September 14 2020 28:45　テスト\n場所 あいうえお\nOct. 2nd, 2022 28:48")).toStrictEqual(
+    {
+      "detail": "",
+      "start": {
+        "tf": true,
+        "year": "2020",
+        "month": 9,
+        "day": "14",
+        "hour": 4,
+        "min": "45",
+      },
+      "end": {
+        "tf": true,
+        "year": "2022",
+        "month": 10,
+        "day": "2",
+        "hour": 4,
+        "min": "48",
+      },
+      "title": "テスト",
+      "location": "あいうえお",
+      "selected_text": "September 14 2020 28:45　テスト\n場所 あいうえお\nOct. 2nd, 2022 28:48",
+    }
+  )
+});
+
+test("デフォルト正規表現テスト_月が英語表記_US2", () => {
+  localStorage.removeItem("detailSwitch");
+  expect(expDefault("Sep 14, 2020 28:45　テスト\n場所 あいうえお\nOct. 2, 2022 28:48")).toStrictEqual(
+    {
+      "detail": "",
+      "start": {
+        "tf": true,
+        "year": "2020",
+        "month": 9,
+        "day": "14",
+        "hour": 4,
+        "min": "45",
+      },
+      "end": {
+        "tf": true,
+        "year": "2022",
+        "month": 10,
+        "day": "2",
+        "hour": 4,
+        "min": "48",
+      },
+      "title": "テスト",
+      "location": "あいうえお",
+      "selected_text": "Sep 14, 2020 28:45　テスト\n場所 あいうえお\nOct. 2, 2022 28:48",
+    }
+  )
+});
+
+test("デフォルト正規表現テスト_月が英語表記_EN1", () => {
+  localStorage.removeItem("detailSwitch");
+  expect(expDefault("14 September 2020 28:45　テスト\n場所 あいうえお\n2nd Oct., 2022 28:48")).toStrictEqual(
+    {
+      "detail": "",
+      "start": {
+        "tf": true,
+        "year": "2020",
+        "month": 9,
+        "day": "14",
+        "hour": 4,
+        "min": "45",
+      },
+      "end": {
+        "tf": true,
+        "year": "2022",
+        "month": 10,
+        "day": "2",
+        "hour": 4,
+        "min": "48",
+      },
+      "title": "テスト",
+      "location": "あいうえお",
+      "selected_text": "14 September 2020 28:45　テスト\n場所 あいうえお\n2nd Oct., 2022 28:48",
+    }
+  )
+});
+
+test("デフォルト正規表現テスト_月が英語表記_EN2", () => {
+  localStorage.removeItem("detailSwitch");
+  expect(expDefault("14 Sep, 2020 28:45　テスト\n場所 あいうえお\n2 Oct., 2022 28:48")).toStrictEqual(
+    {
+      "detail": "",
+      "start": {
+        "tf": true,
+        "year": "2020",
+        "month": 9,
+        "day": "14",
+        "hour": 4,
+        "min": "45",
+      },
+      "end": {
+        "tf": true,
+        "year": "2022",
+        "month": 10,
+        "day": "2",
+        "hour": 4,
+        "min": "48",
+      },
+      "title": "テスト",
+      "location": "あいうえお",
+      "selected_text": "14 Sep, 2020 28:45　テスト\n場所 あいうえお\n2 Oct., 2022 28:48",
+    }
+  )
+});
