e745c12770e4160f306b9131f2544f2f507e186e
retrorocket <1858830+retrorocket@users.noreply.github.com>
2023-12-30 20:10:59 +0900

    テストを追加する

--- __tests__/expdefaultTest.js
+++ __tests__/expdefaultTest.js
@@ -241,3 +241,70 @@ test("デフォルト正規表現テスト_月が英語表記_EN2", () => {
       "14 Sep, 2020 28:45　テスト\n場所 あいうえお\n2 Oct., 2022 28:48",
   });
 });
+
+test("デフォルト正規表現テスト_詳細スイッチ有効_URLスイッチ有効", () => {
+  localStorage.setItem("detailSwitch", true);
+  localStorage.setItem("taburlSwitch", true);
+  expect(
+    expDefault(
+      "2020年9月9日 28:45　テスト\n場所 あいうえお\n2020年9月21日 28:48",
+      "https://example.com?test=test"
+    )
+  ).toStrictEqual({
+    detail:
+      "https://example.com?test=test\n2020年9月9日 28:45　テスト\n場所 あいうえお\n2020年9月21日 28:48",
+    start: {
+      tf: true,
+      year: 2020,
+      month: "9",
+      day: "9",
+      hour: 4,
+      min: "45",
+    },
+    end: {
+      tf: true,
+      year: 2020,
+      month: "9",
+      day: "21",
+      hour: 4,
+      min: "48",
+    },
+    title: "テスト",
+    location: "あいうえお",
+    selected_text:
+      "2020年9月9日 28:45　テスト\n場所 あいうえお\n2020年9月21日 28:48",
+  });
+});
+
+test("デフォルト正規表現テスト_詳細スイッチ無効_URLスイッチ有効", () => {
+  localStorage.removeItem("detailSwitch");
+  localStorage.setItem("taburlSwitch", true);
+  expect(
+    expDefault(
+      "2020年9月9日 28:45　テスト\n場所 あいうえお\n2020年9月21日 28:48",
+      "https://example.com?test=test"
+    )
+  ).toStrictEqual({
+    detail: "https://example.com?test=test",
+    start: {
+      tf: true,
+      year: 2020,
+      month: "9",
+      day: "9",
+      hour: 4,
+      min: "45",
+    },
+    end: {
+      tf: true,
+      year: 2020,
+      month: "9",
+      day: "21",
+      hour: 4,
+      min: "48",
+    },
+    title: "テスト",
+    location: "あいうえお",
+    selected_text:
+      "2020年9月9日 28:45　テスト\n場所 あいうえお\n2020年9月21日 28:48",
+  });
+});
