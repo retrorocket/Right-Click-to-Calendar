3984d7f7df8779d28c9acfb7760552a8a373ad1b
retrorocket <1858830+retrorocket@users.noreply.github.com>
2026-06-04 20:00:25 +0900

    eslint v9対応（やっつけ）

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import jest from "eslint-plugin-jest";
+import globals from "globals";
+import babelParser from "@babel/eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+// FIXME eslint v9が期待通りの挙動にならないので一時的にすべて無効にする
+export default defineConfig([
+  globalIgnores(["app/assets/js/*", "jest*", "coverage"]),
+  {
+    extends: compat.extends("eslint:recommended"),
+
+    plugins: {
+      jest,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.webextensions,
+        ...jest.environments.globals.globals,
+        ...globals.node,
+        luxon: false,
+      },
+
+      parser: babelParser,
+      ecmaVersion: 12,
+      sourceType: "module",
+
+      parserOptions: {
+        requireConfigFile: false,
+
+        babelOptions: {
+          plugins: ["@babel/plugin-syntax-import-assertions"],
+        },
+      },
+    },
+
+    rules: {},
+  },
+]);
