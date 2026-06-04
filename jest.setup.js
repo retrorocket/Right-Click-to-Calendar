42e379002da4a85ae365f1e846b2ba8c0576422a
retrorocket <1858830+retrorocket@users.noreply.github.com>
2026-06-04 19:18:18 +0900

    不要な宣言の削除

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,3 @@
-import { chrome } from '@mobile-next/jest-chrome'
-
-Object.assign(global, require('@mobile-next/jest-chrome'))
+Object.assign(global, require("@mobile-next/jest-chrome"));
 global.luxon = require("luxon");
 global.crypto = require("crypto");
